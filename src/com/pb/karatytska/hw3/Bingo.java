d2c8b8e937b4ad507953c0f9c280ae7025e1f829
anna.karatytskaya <anna.karatytskaya@gmail.com>
2021-10-20 22:09:49 +0300

    Bingo commit

--- src/com/pb/karatytska/hw3/Bingo.java
+++ src/com/pb/karatytska/hw3/Bingo.java
@@ -7,21 +7,21 @@ public class Bingo {
     public static void main (String[] args) {
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
-    int x = rand.nextInt(101);
-        System.out.println("Для выхода из игры введите 000");
-        System.out.println("Введите целое число от 0 до 100: ");
+    int Bingo = rand.nextInt(101);
+        System.out.println("Для выхода из игры введи -1");
+        System.out.println("Введи целое число от 0 до 100: ");
     for (int counter = 1;;){
-        int a = scan.nextInt();
-        if (a == 000) { System.out.println("Game over"); break;}
-                if (a != x) {
+        int User = scan.nextInt();
+        if (User == -1) { System.out.println("Game over");break;}
+        if (User != Bingo) {
             counter ++;
             System.out.println("ne ugadal");
-            if (a > x){
+            if (User > Bingo){
                 System.out.println("Моё число меньше. Попробуй еще раз.");}
-            else if (a < x ) {System.out.println("Моё число больше. Попробуй еще раз.");}
-        }else if(a == x){
+            else if (User < Bingo ) {System.out.println("Моё число больше. Попробуй еще раз.");}
+        }else if(User == Bingo){
             System.out.println("Bingo! Ты угадал число с " + counter + " попытки");
-            System.out.println("Задуманное число:"  +x);
+            System.out.println("Задуманное число:"  +Bingo);
             break;}
         }
     }
